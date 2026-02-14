eca160a8f764cb48a87f3a7363acce13abdc8cda
Manuel Werder <manuelwerder@hotmail.com>
2026-02-14 21:56:07 +0100

    Refactored help command, so no edit after a new command is needed

--- command.go
+++ command.go
@@ -18,13 +18,13 @@ func commandExit() error {
 }
 
 func commandHelp() error {
-	helpMessage := `
-Welcome to the Pokedex!
-Usage:
-
-help: Displays a help help message
-exit: Exit the Pokedex
-`
-	fmt.Println(helpMessage)
+	fmt.Println()
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println()
 	return nil
 }
