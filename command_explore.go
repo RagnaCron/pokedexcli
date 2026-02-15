e6d8bafcff2b884a471de9b090d60e0bdd2782d3
Manuel Werder <manuelwerder@hotmail.com>
2026-02-15 15:48:36 +0100

    Preparing for the explore command

--- command_explore.go
+++ command_explore.go
@@ -1,9 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func commandExplore(conf *cliConfig, para string) error {
-	fmt.Println(para)
+	if para == "" {
+		return errors.New("missing secound parameter")
+	}
+	fmt.Printf("Exploring %v...\n", para)
 
 	return nil
 }
--- internal/pokeapi/locations.go
+++ internal/pokeapi/locations.go
@@ -7,8 +7,6 @@ import (
 	"github.com/RagnaCron/pokedexcli/internal/pokecache"
 )
 
-const baseURL = "https://pokeapi.co/api/v2"
-
 type Locations struct {
 	Count    int     `json:"count"`
 	Next     *string `json:"next"`
@@ -23,7 +21,6 @@ func (r Locations) PrintResults() {
 	for _, location := range r.Results {
 		fmt.Println(location.Name)
 	}
-
 }
 
 func GetLocations(pageURL *string, cache *pokecache.Cache) (Locations, error) {
